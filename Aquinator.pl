6311d9a2591d050fd2c9eec8d7eae94488a17959
lilchris <93929817+lilchris@users.noreply.github.com>
2024-12-15 12:42:52 -0600

    Aquinator Prolog v1

---
+++ Aquinator.pl
@@ -0,0 +1,131 @@
+compania(nintendo).
+compania(microsoft).
+compania(sony).
+compania(asus).
+compania(otra).
+
+%consolas
+consola(nintendo, nintendoGameCube).
+consola(nintendo, nintendo64).
+consola(nintendo, superNintendoEntertainmentSystem).
+consola(nintendo, wii).
+consola(microsoft,xbox).
+consola(microsoft,xboxOne).
+consola(microsoft,xbox360).
+consola(microsoft,xboxSeriesX).
+consola(sony,playStation1).
+consola(sony,playStation2).
+consola(sony,playStation3).
+consola(sony,playStation4).
+consola(sony,playStation5).
+consola(asus,asusROGAlly).
+consola(asus,asusROGAllyX).
+consola(asus,asusROGZephyrus).
+consola(asus,asusROGFlowX13).
+consola(asus,asusROGFlowZ13).
+consola(otra,segaMegaDrive).
+consola(otra,bandaiWonderSwan).
+consola(otra,atari2600).
+consola(otra,gameBoy).
+
+%procesador()
+procesador(nintendoGameCube, iBM_PowerPC_Gekko_128-bit).
+procesador(nintendo64, nEC_VR4300_64-bit_RISC_processor).
+procesador(superNintendoEntertainmentSystem, ricoh_5A22_16-bit).
+procesador(wii, iBMPowerPCBroadway).
+procesador(xbox, "Intel Pentium III 32-bit architecture").
+procesador(xboxOne, "AMD Jaguar APU 8-core 64-bit architecture").
+procesador(xbox360, "IBM PowerPC Xenon 3-core 64-bit architecture").
+procesador(xboxSeriesX, "AMD Zen 2 8-core 64-bit architecture").
+procesador(playStation1,"MIPS R3000A 32-bit architecture").
+procesador(playStation2,"MIPS R5900 Emotion Engine 64-bit architecture").
+procesador(playStation3,"IBM Cell Broadband Engine 8-core 64-bit architecture").
+procesador(playStation4,"AMD Jaguar APU  8-core 64-bit architecture").
+procesador(playStation5,"AMD Zen 2  8-core 64-bit architecture").
+procesador(asusROGAlly, "AMD Ryzen Z1 APU 8 core 64-bit architecture").
+procesador(asusROGAllyX, "AMD Ryzen Z1 Extreme APU 8 core 64-bit architecture").
+procesador(asusROGZephyrus, "AMD Ryzen9 64-bit architecture Intel Core i9 processors").
+procesador(asusROGFlowX13, "AMD Ryzen 9 5900HS  8-core 64-bit architecture").
+procesador(asusROGFlowZ13, "Intel Core i9-12900H   14-core 64-bit architecture").
+procesador(segaMegaDrive, "Motorola 68000 16-bit architecture").
+procesador(bandaiWonderSwan, "Zilog Z80 8-bit architecture").
+procesador(atari2600, "MOS Technology 6507 8-bit architecture").
+procesador(gameBoy, "Sharp LR35902 8-bit architecture").
+
+%year()
+year(nintendoGameCube,2001).
+year(nintendo64,1996).
+year(superNintendoEntertainmentSystem,"1990 en Japon").
+year(superNintendoEntertainmentSystem,"1991 en Norte America").
+year(wii,2006).
+year(xbox, 2001).
+year(xboxOne, 2013).
+year(xbox360, 2005).
+year(xboxSeriesX, 2020).
+year(playStation1, "1994 en Japon").
+year(playStation1, "1995 en Norte America").
+year(playStation2, 2000).
+year(playStation3, 2006).
+year(playStation4, 2013).
+year(playStation5, 2020).
+year(asusROGAlly, 2023).
+year(asusROGAllyX, 2023).
+year(asusROGZephyrus, 2017).
+year(asusROGFlowX13, 2021).
+year(asusROGFlowZ13, 2022).
+year(segaMegaDrive, "1988 en Japon").
+year(segaMegaDrive, "1989 en Norte America").
+year(bandaiWonderSwan, 1999).
+year(atari2600, 1997).
+year(gameBoy, 1989).
+
+%velocidad() MHz
+velocidad(nintendoGameCube, 485).
+velocidad(nintendo64, 93.75).
+velocidad(superNintendoEntertainmentSystem, 3.58).
+velocidad(wii, 729).
+velocidad(xbox, 733).
+velocidad(xboxOne, 1750).
+velocidad(xbox360, 3200).
+velocidad(xboxSeriesX, 3800).
+velocidad(playStation1, 33.87).
+velocidad(playStation2, 294.9).
+velocidad(playStation3, 3200).
+velocidad(playStation4, 1600).
+velocidad(playStation5, 3500).
+velocidad(asusROGAlly, 4700).
+velocidad(asusROGAllyX, 5100).
+velocidad(asusROGZephyrus, 3200).
+velocidad(asusROGFlowX13, 4300).
+velocidad(asusROGFlowZ13, 5000).
+velocidad(segaMegaDrive, 7.61).
+velocidad(bandaiWonderSwan, 3.2).
+velocidad(atari2600, 1.19).
+velocidad(gameBoy, 4.19).
+
+%consumo() watts
+consumo(nintendoGameCube, 39).
+consumo(nintendo64, 15).
+consumo(superNintendoEntertainmentSystem, 17).
+consumo(wii, 20).
+consumo(wii, "9 a 10 watts sin jugar").
+consumo(xbox, 100).
+consumo(xboxOne, 70).
+consumo(xboxOne, 120).
+consumo(xbox360, 100).
+consumo(xbox360, 175).
+consumo(xboxSeriesX, 180).
+consumo(playStation1,10).
+consumo(playStation2,48).
+consumo(playStation3,170).
+consumo(playStation4,165).
+consumo(playStation5,220).
+consumo(asusROGAlly, 15).
+consumo(asusROGAllyX, 15).
+consumo(asusROGZephyrus, 150).
+consumo(asusROGFlowX13, 65).
+consumo(asusROGFlowZ13, 60).
+consumo(segaMegaDrive, 10).
+consumo(bandaiWonderSwan, 2).
+consumo(atari2600, 4).
+consumo(gameBoy, 0.25).
